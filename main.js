af9a1340192efd1ce75658c00d2bbfbaec2156a0
rathin379 <77762779+rathin379@users.noreply.github.com>
2022-02-24 18:02:51 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -54,9 +54,14 @@ function setup(){
     }
   }
   
+  function startGame()
+{
+  game_status = "start";
+  document.getElementById("status").innerHTML = "Game Is Loaded";
+}
 
   function draw(){
-
+    if(game_status == "start");
     background(0); 
     image(video, 0, 0, 700, 600);
   
